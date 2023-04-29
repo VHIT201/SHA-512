725c1cef6c58fede232bea25304b30b9a234e2b5
Phạm Văn Hoàng <88721600+VHIT201@users.noreply.github.com>
2023-04-29 23:22:40 +0700

    update hash

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,22 @@ import { Inter } from "next/font/google";
 import { FiCopy } from "react-icons/fi";
 import { useState } from "react";
 import sha512 from "js-sha512";
+import md5 from "js-md5";
+import sha256 from "js-sha256";
+import sha1 from "js-sha1";
+
+
+import Particles from "react-particles";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Head from "next/head";
+import ParticlesBackground from "@/src/ParticlesBackground";
+import particlesConfig from "@/src/config/particles-config";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const notifyCopy = () =>
-    toast.success("🦄 Copy thành công", {
+    toast.success("Copy thành công", {
       position: "top-right",
       autoClose: 5000,
       hideProgressBar: false,
@@ -21,7 +29,7 @@ export default function Home() {
       theme: "colored",
     });
   const notifyClear = () =>
-    toast.warn("🦄 Xoá thành công", {
+    toast.warn("Xoá thành công", {
       position: "top-right",
       autoClose: 5000,
       hideProgressBar: false,
@@ -31,13 +39,28 @@ export default function Home() {
       progress: undefined,
       theme: "colored",
     });
-
+    const tsParticles = require("tsparticles-engine");
   const [input, setInput] = useState("");
   const [output, setOutput] = useState("");
 
-  const handleHashSha512 = (input) => {
-    const temp = sha512(input);
-    setOutput(temp);
+  const handleHash = (input) => {
+    if (hash.name === "SHA-512") {
+      const temp = sha512(input);
+      setOutput(temp);
+    }
+    if(hash.name === "SHA-1") {
+      const temp = sha1(input);
+      setOutput(temp);
+    }
+    
+    if (hash.name === "SHA-256") {
+      const temp = sha256(input);
+      setOutput(temp);
+    }
+    if (hash.name === "MD5") {
+      const temp = md5(input);
+      setOutput(temp);
+    }
   };
 
   const copyToClipboard = (text) => {
@@ -51,20 +74,99 @@ export default function Home() {
     notifyCopy();
   };
 
+  const [hash, setHash] = useState({
+    name: "SHA-512",
+    link: "https://en.wikipedia.org/wiki/SHA-2",
+    text: "",
+  });
+
   return (
     <>
+    
       <Head>
         <title>Hash Generator</title>
       </Head>
+     
       <main className="flex-col h-screen w-screen bg-slate-400 flex  ">
-        <div className="w-full h-[60vh] bg-[#391F67] gap-4 flex flex-col justify-center items-center">
-          <h1 className="text-white text-[2rem]">
-            Online SHA-512 Hash Generator
+      
+        <div className="w-full h-[60vh]  bg-[#391F67] gap-4 flex flex-col justify-center items-center">
+        
+          <h1 className="text-white text-[3rem]">
+            Online {hash.name} Hash Generator
           </h1>
-          <h1 className="text-white text-[1.2rem]">
-            Simple tool from engineers for engineers to easily compute SHA-512
-            hash of any string
+          
+          <h1 className="text-white text-[1.6rem]">
+            Simple tool from engineers for engineers to easily compute{" "}
+            {hash.name }
+             {" "}hash of any string
           </h1>
+          <div className="flex gap-4 mt-6">
+            <button
+              onClick={() =>
+                setHash({
+                  name: "MD5",
+                  link: "https://vi.wikipedia.org/wiki/MD5",
+                  text: "MD5 Message Digest Algorithm 5 is a widely used cryptographic hash function that produces a 128-bit 16-byte hash value. It is commonly used for checking data integrity, verifying message authentication, and storing passwords. MD5 is a one-way function, meaning it is not reversible and cannot be decrypted back to the original input data. Despite its widespread use in the past, MD5 has been considered insecure in recent years due to vulnerabilities and collisions found in the algorithm.",
+                })
+              }
+              className={`${
+                hash.name === "MD5"
+                  ? "bg-sky-600 rounded-full text-white  border border-white px-10 py-2"
+                  : "rounded-full text-white  border border-white px-10 py-2 hover:translate-y-[-2px] hover:bg-neutral-400" 
+              }`}
+            >
+              MD5
+            </button>
+            <button
+              onClick={() =>
+                setHash({
+                  name: "SHA-1",
+                  link: "https://vi.wikipedia.org/wiki/SHA-1",
+                  text: "SHA-1 is a cryptographic hash function that generates a 160-bit `20-byte` hash value, typically rendered as a hexadecimal number. It is no longer considered secure for cryptographic purposes due to vulnerabilities found in the algorithm. SHA-2, which includes SHA-256, SHA-384, and SHA-512, is currently recommended for most cryptographic applications.",
+                })
+              }
+              className={`${
+                hash.name === "SHA-1"
+                  ? "bg-sky-600 rounded-full text-white  border border-white px-10 py-2"
+                  : "hover:bg-neutral-400 rounded-full text-white  border border-white px-10 py-2 hover:translate-y-[-2px]"
+              }`}
+            >
+              SHA-1
+            </button>
+            
+            <button
+              onClick={() =>
+                setHash({
+                  name: "SHA-256",
+                  link: "https://en.wikipedia.org/wiki/SHA-2",
+                  text: "SHA-256 `Secure Hash Algorithm 256` is a cryptographic hash function that takes an input and generates a fixed-size 256-bit `32-byte` hash value. It is a widely used hash function and is considered to be secure for most cryptographic purposes, including digital signatures and message authentication codes `MACs`. SHA-256 is a member of the SHA-2 family of hash functions, which also includes SHA-224, SHA-384, and SHA-512. It was designed by the National Security Agency `NSA` and published by the National Institute of Standards and Technology `NIST` in 2001.",
+                })
+              }
+              className={`${
+                hash.name === "SHA-256"
+                  ? "bg-sky-600 rounded-full text-white  border border-white px-10 py-2"
+                  : "hover:bg-neutral-400 rounded-full text-white  border border-white px-10 py-2 hover:translate-y-[-2px]"
+              }`}
+            >
+              SHA-256
+            </button>
+            <button
+              onClick={() =>
+                setHash({
+                  name: "SHA-512",
+                  link: "https://en.wikipedia.org/wiki/SHA-2",
+                  text: "SHA-512 Cryptographic Hash Algorithm. A cryptographic hash `sometimes called `digest`` is a kind of `signature` for a text or a data file. SHA-512 generates an almost-unique 512-bit `32-byte` signature for a text.",
+                })
+              }
+              className={`${
+                hash.name === "SHA-512"
+                  ? "bg-sky-600 rounded-full text-white  border border-white px-10 py-2"
+                  : "hover:bg-neutral-400 rounded-full text-white  border border-white px-10 py-2 hover:translate-y-[-2px]"
+              }`}
+            >
+              SHA-512
+            </button>
+          </div>
         </div>
         <section className="flex flex-col lg:flex-row w-full py-12 px-8 bg-white justify-between">
           <div className="lg:w-[49%] w-full">
@@ -80,7 +182,7 @@ export default function Home() {
               <div className="flex flex-row w-full justify-evenly  ">
                 <button
                   onClick={() => {
-                    handleHashSha512(input);
+                    handleHash(input);
                   }}
                   className="hover:bg-sky-600 transition-all duration-300 hover:translate-y-[-2px] cursor-pointer bg-sky-700 px-6 py-2 rounded-lg text-white font-bold"
                 >
@@ -102,7 +204,7 @@ export default function Home() {
             {/* output */}
             <div className="w-full flex flex-col gap-2 p-4 border py-8 border-sky-500 rounded-lg mt-10 ">
               <label className="font-bold text-neutral-600">
-                SHA-512 OUTPUT:
+                {hash.name} OUTPUT:
               </label>
               <span className="my-4 break-all h-full text-left w-full font-thin text-[24px]">
                 {output}
@@ -120,17 +222,12 @@ export default function Home() {
           </div>
           <div className="lg:w-[1px] w-0 bg-black h-0 "></div>
           <div className="lg:w-[49%] w-full px-8 ">
-            <h1 className="text-[2rem] font-thin">What is SHA-512?</h1>
-            <p className="text-[1rem] text-neutral-500 my-6">
-              SHA-512 Cryptographic Hash Algorithm. A cryptographic hash
-              sometimes called digest is a kind of signature for a text or a
-              data file. SHA-512 generates an almost-unique 512-bit 32-byte
-              signature for a text.
-            </p>
+            <h1 className="text-[3rem] font-thin">What is {hash.name}?</h1>
+            <p className="text-[1.5rem] text-neutral-500 my-6">{hash.text}</p>
             <a
-              className="text-sky-600 font-semibold"
+              className="text-sky-600 font-semibold text-[1.5rem]"
               target={"_blank"}
-              href="https://en.wikipedia.org/wiki/SHA-2"
+              href={hash.link}
             >
               Learn more »
             </a>
